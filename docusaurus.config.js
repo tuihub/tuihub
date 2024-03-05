22078628ce92cd39cbd3775d57d8a8d6ac22c6a1
Tabing010102 <tabing010102@outlook.com>
2024-03-05 23:22:58 +0800

    fix: enable csharp code highlighting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,7 +136,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['bash'],
+        additionalLanguages: ['bash', 'csharp'],
       },
     }),
 };
